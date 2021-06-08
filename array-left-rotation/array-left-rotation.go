0ce062827fe431e4e487da70cde29314cfd5f8fb
Yuda Cogati <yuda.pc@gmail.com>
2021-06-08 22:48:39 +0700

    add array left rotation

---
+++ array-left-rotation/array-left-rotation.go
@@ -0,0 +1,21 @@
+package arrayleftrotation
+
+func ArrayLeftRotation(a []int32, d int32) []int32 {
+	rotation := d % int32(len(a))
+	if rotation == 0 {
+		return a
+	}
+
+	arr1 := make([]int32, 0)
+	arr2 := make([]int32, 0)
+
+	for i := range a {
+		if i < int(rotation) {
+			arr1 = append(arr1, a[i])
+		} else {
+			arr2 = append(arr2, a[i])
+		}
+	}
+
+	return append(arr2, arr1...)
+}
---
+++ array-left-rotation/array-left-rotation_test.go
@@ -0,0 +1,15 @@
+package arrayleftrotation
+
+import (
+	"reflect"
+	"testing"
+)
+
+func TestArrayLeftRotation(t *testing.T) {
+	input := []int32{1, 2, 3, 4, 5}
+	expected := []int32{3, 4, 5, 1, 2}
+	result := ArrayLeftRotation(input, 2)
+	if !reflect.DeepEqual(result, expected) {
+		t.Error("The result should be ", expected)
+	}
+}
