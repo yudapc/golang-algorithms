ba8c0ecd534e75c9a76e5d6bfa166df47a916312
Yuda Cogati <yuda.pc@gmail.com>
2021-06-24 22:54:56 +0700

    add test benchmark performance

--- array-left-rotation/array-left-rotation_test.go
+++ array-left-rotation/array-left-rotation_test.go
@@ -22,3 +22,23 @@ func TestArrayLeftRotationSimple(t *testing.T) {
 		t.Error("The result should be ", expected)
 	}
 }
+
+func BenchmarkArrayLeftRotation(b *testing.B) {
+	input := []int32{1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20}
+	for i := 0; i < b.N; i++ {
+		if i > len(input) {
+			break
+		}
+		ArrayLeftRotation(input, int32(i))
+	}
+}
+
+func BenchmarkArrayLeftRotationSimple(b *testing.B) {
+	input := []int32{1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20}
+	for i := 0; i < b.N; i++ {
+		if i > len(input) {
+			break
+		}
+		ArrayLeftRotationSimple(input, int32(i))
+	}
+}
