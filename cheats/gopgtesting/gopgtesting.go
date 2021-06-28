c60941a9aab1f562cb658b2a874752284e21d16d
Yuda Cogati <yuda.pc@gmail.com>
2021-06-28 12:23:30 +0700

    change database host, port, name, user, pass to file .env

--- cheats/gopgtesting/gopgtesting.go
+++ cheats/gopgtesting/gopgtesting.go
@@ -3,16 +3,20 @@ package gopgtesting
 import (
 	"context"
 	"fmt"
+	"os"
 
 	"github.com/go-pg/pg/v10"
+	"github.com/joho/godotenv"
 )
 
 func NewDBConnection() *pg.DB {
-	username := "postgres"
-	password := "12345678"
-	port := "5432"
-	database := "gopgtesting"
-	dbUrl := fmt.Sprintf("postgres://%s:%s@localhost:%s/%s?sslmode=disable", username, password, port, database)
+	godotenv.Load(".env")
+	username := os.Getenv("DB_USER")
+	password := os.Getenv("DB_PASS")
+	port := os.Getenv("DB_PORT")
+	database := os.Getenv("DB_NAME")
+	host := os.Getenv("DB_HOST")
+	dbUrl := fmt.Sprintf("postgres://%s:%s@%s:%s/%s?sslmode=disable", username, password, host, port, database)
 	opt, err := pg.ParseURL(dbUrl)
 	if err != nil {
 		panic(err)
--- go.mod
+++ go.mod
@@ -8,4 +8,5 @@ require (
 	github.com/go-pg/pg/v10 v10.10.0 // indirect
 	github.com/google/uuid v1.2.0 // indirect
 	github.com/gorilla/mux v1.8.0 // indirect
+	github.com/joho/godotenv v1.3.0 // indirect
 )
