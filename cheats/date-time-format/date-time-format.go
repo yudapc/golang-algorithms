ff94cae50db7e8c063293be1faa692676144b67f
Yuda Cogati <yuda.pc@gmail.com>
2021-07-06 07:27:17 +0700

    add convert am pm to 24 hour

--- cheats/date-time-format/date-time-format.go
+++ cheats/date-time-format/date-time-format.go
@@ -28,3 +28,12 @@ func DateTimeFormatISO8601(inputDateTime string) string {
 	t, _ := time.Parse(layoutISO, inputDateTime)
 	return t.Format(FORMAT)
 }
+
+func DateTimeFormatTo24(inputDateTime string) string {
+	const (
+		layoutISO = "2006-01-02 03:04:05PM"
+		FORMAT    = "02-01-2006 15:04"
+	)
+	t, _ := time.Parse(layoutISO, inputDateTime)
+	return t.Format(FORMAT)
+}
--- cheats/date-time-format/date-time-format_test.go
+++ cheats/date-time-format/date-time-format_test.go
@@ -7,7 +7,10 @@ import (
 func TestDateFormat(t *testing.T) {
 	date := "2016-11-28"
 	dateFormated := DateFormat(date)
-	if dateFormated != "28/11/2016" {
+	expected := "28/11/2016"
+	if dateFormated != expected {
+		t.Error("Received:", dateFormated)
+		t.Error("Expected:", expected)
 		t.Error("your date format is wrong", dateFormated)
 	}
 }
@@ -15,7 +18,10 @@ func TestDateFormat(t *testing.T) {
 func TestDateTimeFormat(t *testing.T) {
 	date := "2016-11-28 18:09:02"
 	dateFormated := DateTimeFormat(date)
-	if dateFormated != "28/11/2016 18:09" {
+	expected := "28/11/2016 18:09"
+	if dateFormated != expected {
+		t.Error("Received:", dateFormated)
+		t.Error("Expected:", expected)
 		t.Error("your date time format is wrong", dateFormated)
 	}
 }
@@ -23,7 +29,21 @@ func TestDateTimeFormat(t *testing.T) {
 func TestDateTimeFormatISO8601(t *testing.T) {
 	date := "2021-06-02T15:04:05+0700"
 	dateFormated := DateTimeFormatISO8601(date)
-	if dateFormated != "02/06/2021 15:04 WIB" {
+	expected := "02/06/2021 15:04 WIB"
+	if dateFormated != expected {
+		t.Error("Received:", dateFormated)
+		t.Error("Expected:", expected)
 		t.Error("your date time format ISO8601 is wrong", dateFormated)
+	}
+}
+
+func TestDateTimeFormatAMPMTo24(t *testing.T) {
+	date := "2021-06-02 04:04:00PM"
+	dateFormated := DateTimeFormatTo24(date)
+	expected := "02-06-2021 16:04"
+	if dateFormated != expected {
+		t.Error("Received:", dateFormated)
+		t.Error("Expected:", expected)
+		t.Error("your date time format 24 is wrong", dateFormated)
 	}
 }
